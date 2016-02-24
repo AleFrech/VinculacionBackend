8be01fcb361f1b2b6bce78ee9e4ae467df423b9f
carlosv14 <carlos.varela@unitec.edu>
2016-02-23 18:11:33 -0600

    Added Entity

---
+++ VinculacionBackend/VinculacionBackend/Database/VinculacionContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace VinculacionBackend.Database
+{
+    public class VinculacionContext:DbContext
+    {
+        public VinculacionContext() : base(ConnectionString.Get())
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+
+    public static class ConnectionString
+    {
+        public static string Get()
+        {
+            var Environment = ConfigurationManager.AppSettings["Environment"];
+            return string.Format("name={0}", Environment);
+        }
+    }
+
+}
