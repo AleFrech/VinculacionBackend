47c57d6bc39e22980a5b8af835d22780418c8829
edwherrera <edwherrera.175@gmail.com>
2016-02-24 17:01:54 -0600

    EmailExists method added

--- VinculacionBackend/VinculacionBackend/EntityExistanceManager.cs
+++ VinculacionBackend/VinculacionBackend/EntityExistanceManager.cs
@@ -7,5 +7,10 @@ namespace VinculacionBackend
 {
     public class EntityExistanceManager
     {
+        public static bool EmailExists(string email)
+        {
+            var context = new VinculacionBackend.Database.VinculacionContext();
+            return context.Students.Any(x => x.Email == email);
+        }
     }
 }
