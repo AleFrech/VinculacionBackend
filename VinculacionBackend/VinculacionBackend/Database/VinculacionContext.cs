df2c03cdbc3109937437707fa19e3d26dbfd4d20
edwherrera <edwherrera.175@gmail.com>
2016-02-25 23:12:46 -0600

    Role and User_Role entities added

--- VinculacionBackend/VinculacionBackend/Database/VinculacionContext.cs
+++ VinculacionBackend/VinculacionBackend/Database/VinculacionContext.cs
@@ -8,15 +8,18 @@ using VinculacionBackend.Entities;
 
 namespace VinculacionBackend.Database
 {
-    public class VinculacionContext:DbContext
+    public class VinculacionContext : DbContext
     {
         public VinculacionContext() : base(ConnectionString.Get())
         {
-            
+
         }
 
         public DbSet<Student> Students { get; set; }
         public DbSet<Major> Majors { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User_Role> User_Role_Rels{get; set; }
+        public DbSet<Major_User> Major_Users_Rels { get; set; }
     }
 
     public static class ConnectionString
---
+++ VinculacionBackend/VinculacionBackend/Entities/User_Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VinculacionBackend.Entities
+{
+    public class User_Role
+    {
+        public long Id { get; set; }
+        public User User { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
