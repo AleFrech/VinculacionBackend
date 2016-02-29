c35ea0939e02a729d3c17f0880bf3040790e7113
Alejandro-Frech <alefrech@gmail.com>
2016-02-29 16:05:02 -0600

    using HourEntryModel in HourApi

---
+++ VinculacionBackend/VinculacionBackend/Models/HourEntryModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VinculacionBackend.Models
+{
+    public class HourEntryModel
+    {
+        public string NumberId { get; set; }
+
+        public long SectionId { get; set; }
+
+        public long ProyectId{ get; set; }
+       
+        public int Hour { get; set; }
+    }
+}
