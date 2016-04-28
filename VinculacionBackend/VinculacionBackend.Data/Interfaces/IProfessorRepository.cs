c0d422f6edd20f18107ab4da2b74312918e19e69
edwherrera <edwherrera.175@gmail.com>
2016-04-27 19:32:35 -0600

    basic ProfessorRepository added

---
+++ VinculacionBackend/VinculacionBackend.Data/Interfaces/IProfessorRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using VinculacionBackend.Data.Entities;
+using VinculacionBackend.Data.Enums;
+
+namespace VinculacionBackend.Data.Interfaces
+{
+	public interface IProfessorRepository : IRepository<User>
+	{
+		
+	}
+}
