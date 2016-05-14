c895bf5d1261f9a922f8fda8145bde8eb984cd1d
Mario Villatoro <Mario Villatoro>
2016-05-14 12:30:17 -0600

    fix cathing

--- VinculacionBackend/VinculacionBackend/Controllers/MajorsController.cs
+++ VinculacionBackend/VinculacionBackend/Controllers/MajorsController.cs
@@ -2,11 +2,10 @@ using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Description;
 using System.Web.Http.Cors;
-using System.Web.Mvc;
 using System.Web.OData;
-using System.Web.UI;
 using VinculacionBackend.Data.Entities;
 using VinculacionBackend.Interfaces;
+using WebApi.OutputCache.V2.TimeAttributes;
 
 namespace VinculacionBackend.Controllers
 {
@@ -20,16 +19,18 @@ namespace VinculacionBackend.Controllers
         }
 
         // GET: api/Majors
-        [System.Web.Http.Route("api/Majors")]
         [EnableQuery]
+        [CacheOutputUntilToday(23, 59)]
+        [System.Web.Http.Route("api/Majors")]
         public IQueryable<Major> GetMajors()
         {
             return _majorsServices.All();
         }
 
         // GET: api/Majors/5
-        [OutputCache(Duration = 7200, VaryByParam = "none" , Location = OutputCacheLocation.Client, NoStore = false)]
+        [ActionName("GetMajorId")]
         [ResponseType(typeof(Major))]
+        [CacheOutputUntilToday(23,59)]
         [System.Web.Http.Route("api/Majors/{majorId}")]
         public IHttpActionResult GetMajor(string majorId)
         {
