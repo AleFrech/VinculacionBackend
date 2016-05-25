2a55c94bba69adc009725108472b91ab3b4250c3
Alejandro-Frech <alefrech@gmail.com>
2016-05-24 18:06:20 -0600

    fixed update in studentsRepository

--- VinculacionBackend/VinculacionBackend.Data/Repositories/StudentRepository.cs
+++ VinculacionBackend/VinculacionBackend.Data/Repositories/StudentRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using VinculacionBackend.Data.Database;
 using VinculacionBackend.Data.Entities;
@@ -115,7 +116,7 @@ namespace VinculacionBackend.Data.Repositories
 
         public void Update(User ent)
         {
-            _db.Entry(ent).State = EntityState.Modified;
+            _db.Users.AddOrUpdate(ent);
         }
 
         private IEnumerable<UserRole> GetUserRoleRelationships()
