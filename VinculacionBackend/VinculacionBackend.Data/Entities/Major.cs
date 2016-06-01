a095072fd08f23677ec4a7477091895d18df67d6
edwherrera <edwherrera.175@gmail.com>
2016-05-31 19:26:19 -0600

    Faculties and Class-Major rel added

--- VinculacionBackend/VinculacionBackend.Data/Entities/Major.cs
+++ VinculacionBackend/VinculacionBackend.Data/Entities/Major.cs
@@ -5,5 +5,6 @@
         public long Id { get; set; }
         public string MajorId { get; set; }
         public string Name { get; set; }       
+        public Faculty Faculty { get; set; }
     }
 }
---
+++ VinculacionBackend/VinculacionBackend.Data/Entities/MajorClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinculacionBackend.Data.Entities
+{
+    public class MajorClass
+    {
+        public long Id { get; set; }
+        public Major Major { get; set; }
+        public Class Class { get; set; }
+    }
+}
