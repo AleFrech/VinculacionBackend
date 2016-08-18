018a15f35aa93014b7fd72ccfe26f8fc446d8d02
Edwin Herrera <eherrera@Edwins-MacBook-Pro.local>
2016-08-17 21:21:09 -0600

    added SectionProjectInfo'

---
+++ VinculacionBackend/VinculacionBackend/Models/SectionProjectInfoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VinculacionBackend.Models
+{
+    public class SectionProjectInfoModel
+    {
+        public string ProjectName;
+        public string ClassName;
+        public string ProfessorName;
+    }
+}
---
+++ VinculacionBackend/VinculacionBackend/Services/SectionProjectServices.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VinculacionBackend.Data.Interfaces;
+using VinculacionBackend.Data.Repositories;
+using VinculacionBackend.Exceptions;
+using VinculacionBackend.Interfaces;
+using VinculacionBackend.Models;
+
+namespace VinculacionBackend.Services
+{
+    public class SectionProjectServices : ISectionProjectServices
+    {
+        private readonly ISectionProjectRepository _sectionProjectRepository;
+
+        public SectionProjectServices()
+        {
+            _sectionProjectRepository = new SectionProjectRepository();
+        }
+
+        public SectionProjectInfoModel GetInfo(long id)
+        {
+            var sectionProject = _sectionProjectRepository.Get(id);
+            if(sectionProject == null)
+                throw new NotFoundException(id + " not found");
+            return new SectionProjectInfoModel
+            {
+                ClassName = sectionProject.Section.Class.Name,
+                ProfessorName = sectionProject.Section.User.Name,
+                ProjectName = sectionProject.Project.Name
+            };
+        }
+    }
+}
