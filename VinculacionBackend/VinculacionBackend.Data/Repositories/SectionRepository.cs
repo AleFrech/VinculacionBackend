4280d1e1fced19f1b1f32366d50eed8a0a60a581
Edwin Herrera <edwherrera.175@gmail.com>
2016-09-07 19:26:28 -0600

    ....

--- VinculacionBackend/VinculacionBackend.Data/Repositories/SectionRepository.cs
+++ VinculacionBackend/VinculacionBackend.Data/Repositories/SectionRepository.cs
@@ -119,7 +119,7 @@ namespace VinculacionBackend.Data.Repositories
 
         public void ClearSectionStudents(long sectionId)
         {
-            var sectionStudents = _db.SectionProjectsRels.Include(x => x.Section).Where(x => x.Section.Id == sectionId);
+            List<SectionUser> sectionStudents = _db.SectionProjectsRels.Include(x => x.Section).Where(x => x.Section.Id == sectionId);
             foreach(var rel in sectionStudents)
             {
                 _db.SectionUserRels.Remove(rel);
